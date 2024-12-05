0722843c9aca5a89f34416809e06ada40c2e7a59
David Ferreira Lima <dfl8@aluno.ifal.edu.br>
2024-12-05 00:13:27 -0300

    init: food explorer iOS project creation

---
+++ Food-explorer/Food-explorerTests/Food_explorerTests.swift
@@ -0,0 +1,17 @@
+//
+//  Food_explorerTests.swift
+//  Food-explorerTests
+//
+//  Created by David Ferreira Lima on 05/12/24.
+//
+
+import Testing
+@testable import Food_explorer
+
+struct Food_explorerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}

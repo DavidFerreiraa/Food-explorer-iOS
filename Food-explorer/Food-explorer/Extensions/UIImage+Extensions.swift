371ddeb07648a36e0c0a13c48b6db56e42938b0b
David Ferreira Lima <dfl8@aluno.ifal.edu.br>
2024-12-06 00:39:33 -0300

    feat: add a new extension to deal with the UIImages

---
+++ Food-explorer/Food-explorer/Extensions/UIImage+Extensions.swift
@@ -0,0 +1,13 @@
+//
+//  UIImage+Extensions.swift
+//  Food-explorer
+//
+//  Created by David Ferreira Lima on 05/12/24.
+//
+
+import Foundation
+import UIKit
+
+extension UIImage {
+    static let logoIcon = UIImage(named: "AppIconStaticImage")
+}

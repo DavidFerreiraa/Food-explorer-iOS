ce36649bfa9fcbea03ee744c439d71377faec734
David Ferreira Lima <dfl8@aluno.ifal.edu.br>
2024-12-07 00:48:35 -0300

    feat: add a new ExplorerTextFieldWithLabelStackView

--- Food-explorer/Food-explorer/Views/Components/ExplorerTextFieldWithLabelStackView.swift
+++ Food-explorer/Food-explorer/Views/Components/ExplorerTextFieldWithLabelStackView.swift
@@ -7,8 +7,51 @@
 
 import UIKit
 
-class ExplorerLabelWrapperStackView: UIStackView {
-    init() {
+class ExplorerTextFieldWithLabelStackView: UIStackView {
+    let textField: ExplorerTextField
+    let labelText: String
+    init(labelText: String, textField: ExplorerTextField) {
+        self.textField = textField
+        self.labelText = labelText
         
+        super.init(frame: .zero)
+        axis = .vertical
+        spacing = 8
+        alignment = .leading
+        distribution = .fill
+        setupView()
+    }
+    
+    required init(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private lazy var label: UILabel = {
+        let label = UILabel()
+        label.text = self.labelText
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = .roboto(.regular, size: 16)
+        label.textColor = .light400
+        return label
+    }()
+    
+    private func setupView() {
+        setHierarchy()
+        setConstraints()
+    }
+    
+    private func setHierarchy() {
+        addArrangedSubview(label)
+        addArrangedSubview(textField)
+    }
+    
+    private func setConstraints() {
+        NSLayoutConstraint.activate([
+            self.widthAnchor.constraint(equalToConstant: 316),
+            textField.heightAnchor.constraint(equalToConstant: 44),
+            textField.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            textField.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+            label.heightAnchor.constraint(equalToConstant: 16)
+        ])
     }
 }

e57a974d2b1e02aa0e4d90ede820d1933cf11f7f
David Ferreira Lima <dfl8@aluno.ifal.edu.br>
2024-12-07 00:48:01 -0300

    feat: add a new component ExplorerTextField

--- Food-explorer/Food-explorer/Views/Components/ExplorerTextField.swift
+++ Food-explorer/Food-explorer/Views/Components/ExplorerTextField.swift
@@ -8,13 +8,27 @@
 import UIKit
 
 class ExplorerTextField: UITextField {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    init(placeholderText: String) {
+        super.init(frame: .zero)
+        translatesAutoresizingMaskIntoConstraints = false
+        keyboardType = .default
+        backgroundColor = .dark900
+        textColor = .light500
+        font = .roboto(.regular, size: 16)
+        borderStyle = .roundedRect
+        let placeholderAttributes: [NSAttributedString.Key: Any] = [
+            .font: UIFont.roboto(.regular, size: 16),
+            .foregroundColor: UIColor.light500
+        ]
+        attributedPlaceholder = NSAttributedString(string: placeholderText, attributes: placeholderAttributes)
+        
     }
-    */
-
+    
+    override func textRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.inset(by: UIEdgeInsets(top: 12, left: 16, bottom: 12, right: 16))
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }

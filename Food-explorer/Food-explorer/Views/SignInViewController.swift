89df1d472d522fa0fd5e7216a85a6d29df85e85a
David Ferreira Lima <dfl8@aluno.ifal.edu.br>
2024-12-07 00:49:41 -0300

    feat: apply all the new components in the SignInViewController

--- Food-explorer/Food-explorer/Views/SignInViewController.swift
+++ Food-explorer/Food-explorer/Views/SignInViewController.swift
@@ -9,14 +9,61 @@ import UIKit
 
 class SignInViewController: UIViewController {
     
-    private lazy var logoView: LogoView = LogoView(logoSize: 38)
-    
     private lazy var backgroundView: UIStackView = {
-        let view = UIStackView(arrangedSubviews: [logoView])
+        let view = UIStackView(arrangedSubviews: [logoView, inputWrapperView, UIView()])
+        view.axis = .vertical
         view.translatesAutoresizingMaskIntoConstraints = false
         view.isLayoutMarginsRelativeArrangement = true
-        view.directionalLayoutMargins = NSDirectionalEdgeInsets(top: 158, leading: 48, bottom: 0, trailing: 48)
+        view.directionalLayoutMargins = NSDirectionalEdgeInsets(top: 164, leading: 0, bottom: 0, trailing: 0)
+        view.alignment = .center
+        view.distribution = .fill
+        view.spacing = 73
         return view
+    }()
+    
+    private lazy var logoView: LogoView = LogoView(logoSize: 38)
+    
+    private lazy var inputWrapperView: UIStackView = {
+        let view = UIStackView(arrangedSubviews: [explorerEmailTextFieldWithLabelStackView, explorerPasswordTextFieldWithLabelStackView, explorerButton, explorerOnlyTextButton])
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.axis = .vertical
+        view.spacing = 32
+        view.distribution = .fillProportionally
+        view.alignment = .center
+
+        return view
+    }()
+    
+    private lazy var explorerEmailTextField: ExplorerTextField = {
+        let textField = ExplorerTextField(placeholderText: "Exemplo: exemplo@exemplo.com")
+        textField.center = view.center
+        return textField
+    }()
+    
+    private lazy var explorerPasswordTextField: ExplorerTextField = {
+        let textField = ExplorerTextField(placeholderText: "No mínimo 6 caracteres")
+        textField.center = view.center
+        return textField
+    }()
+    
+    private lazy var explorerEmailTextFieldWithLabelStackView: ExplorerTextFieldWithLabelStackView = {
+        let view = ExplorerTextFieldWithLabelStackView(labelText: "Email", textField: explorerEmailTextField)
+        return view
+    }()
+    
+    private lazy var explorerPasswordTextFieldWithLabelStackView: ExplorerTextFieldWithLabelStackView = {
+        let view = ExplorerTextFieldWithLabelStackView(labelText: "Senha", textField: explorerPasswordTextField)
+        return view
+    }()
+    
+    private lazy var explorerButton: ExplorerButton = {
+        let button = ExplorerButton(buttonText: "Entrar")
+        return button
+    }()
+    
+    private lazy var explorerOnlyTextButton: ExplorerButton = {
+        let button = ExplorerButton(buttonText: "Criar uma conta", onlyText: true)
+        return button
     }()
 
     override func viewDidLoad() {
@@ -36,11 +83,7 @@ class SignInViewController: UIViewController {
     
     private func setConstraints() {
         backgroundView.setConstraintsToParent(view)
-        
-        NSLayoutConstraint.activate([
-            logoView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-            logoView.topAnchor.constraint(equalTo: view.topAnchor, constant: 164),
-        ])
+        logoView.heightAnchor.constraint(equalToConstant: 44).isActive = true
     }
 }
 

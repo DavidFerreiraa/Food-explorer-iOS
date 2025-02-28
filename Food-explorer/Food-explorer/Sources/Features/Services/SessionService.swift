dbcc04261ca657daebc35b998350053896127701
David Ferreira Lima <dfl8@aluno.ifal.edu.br>
2025-02-28 00:14:08 -0300

    feat: add SessionService

--- Food-explorer/Food-explorer/Sources/Features/Services/SessionService.swift
+++ Food-explorer/Food-explorer/Sources/Features/Services/SessionService.swift
@@ -6,3 +6,66 @@
 //
 
 import Foundation
+
+enum SessionServiceError: Error {
+    case authenticationFailed
+}
+
+class SessionService {
+    let userServiceEndpoint: String = Api.baseUrlString + "/sessions"
+    static let shared = SessionService()
+    
+    private let session = URLSession.shared
+    
+    func createSession(email: String, password: String, completion: @escaping (Result<User, Error>) -> Void) {
+        
+        guard let url = URL(string: userServiceEndpoint) else {
+            return completion(.failure(ApiError.invalidURL))
+        }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        let body: [String: String] = [
+            "email": email,
+            "password": password
+        ]
+        
+        request.httpBody = try? JSONEncoder().encode(body)
+        
+        session.dataTask(with: request) {
+            data, response, error in
+            if let error = error {
+                print("Erro de rede:", error.localizedDescription)
+                return completion(.failure(error))
+            }
+            
+            guard let data else {
+                print("Erro: Nenhum dado recebido")
+                return completion(.failure(ApiError.noDataReceived))
+            }
+            
+            guard let response = response as? HTTPURLResponse else {
+                print("Erro: Resposta inválida do servidor")
+                return completion(.failure(ApiError.invalidResponse))
+            }
+            
+            switch response.statusCode {
+            case 201:
+            if let userData = try? JSONDecoder().decode(User.self, from: data) {
+                return completion(.success(userData))
+            } else {
+                print("Erro: Dados de usuário inválidos")
+                return completion(.failure(ApiError.invalidData))
+            }
+            case 401:
+                print("Erro: Autenticação falhou")
+                return completion(.failure(SessionServiceError.authenticationFailed))
+            default:
+                print("Erro: Status code inesperado: \(response.statusCode)")
+                return completion(.failure(ApiError.invalidResponse))
+            }
+        }.resume()
+    }
+}

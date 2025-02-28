1eac8dcd7bf9f7531dde1340edc97e43865705c2
David Ferreira Lima <dfl8@aluno.ifal.edu.br>
2025-02-28 00:15:13 -0300

    fix: removing unused cases into UserService

--- Food-explorer/Food-explorer/Sources/Features/Services/UserService.swift
+++ Food-explorer/Food-explorer/Sources/Features/Services/UserService.swift
@@ -7,24 +7,16 @@
 
 import Foundation
 
-enum UserServiceError: Error {
-    case invalidURL
-    case invalidResponse
-    case invalidData
-    case noResponseReceived
-    case noDataReceived
-}
-
 class UserService {
     let userServiceEndpoint: String = Api.baseUrlString + "/users"
     static let shared: UserService = UserService()
     
     private let session = URLSession.shared
     
-    func createUser(name: String, email: String, password: String, _ completion: @escaping (Result <Void, Error>) -> Void) throws {
+    func createUser(name: String, email: String, password: String, _ completion: @escaping (Result <Void, Error>) -> Void) {
         
         guard let url = URL(string: userServiceEndpoint) else {
-            return completion(.failure(UserServiceError.invalidURL))
+            return completion(.failure(ApiError.invalidURL))
         }
         
         var request = URLRequest(url: url)
@@ -47,12 +39,12 @@ class UserService {
             
             guard let data else {
                 print("Erro: Nenhum dado recebido")
-                return completion(.failure(UserServiceError.noDataReceived))
+                return completion(.failure(ApiError.noDataReceived))
             }
 
-            guard let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 201 else {
+            guard let response = response as? HTTPURLResponse, response.statusCode == 201 else {
                 print("Erro: Resposta inválida do servidor")
-                return completion(.failure(UserServiceError.invalidResponse))
+                return completion(.failure(ApiError.invalidResponse))
             }
 
             completion(.success(()))
